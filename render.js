62782d3719c36a3ccc24134bd0d80e94de5601e1
komalessarani <kessara0@live.unc.edu>
2019-12-11 14:20:53 -0500

    fixed typo

--- render.js
+++ render.js
@@ -1355,7 +1355,7 @@ export const renderProfile = async function () {
                     <div class="content">
                             <b>Username:  </b>   ${stat.data.user.name}
                             <br><br>
-                            <b>Username:  </b>   ${user.email}
+                            <b>Email:  </b>   ${user.email}
                             <br><br>
                             <b>Name:  </b>   ${user.firstname} ${user.lastname}
                             <br><br>
